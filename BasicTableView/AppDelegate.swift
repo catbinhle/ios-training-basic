95ed364617fb3532d6b8e19411a13fb1e250b893
Cat Binh Le <catbinhle@gmail.com>
2020-09-06 19:30:44 +0700

    no message

--- BasicTableView/AppDelegate.swift
+++ BasicTableView/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     //Test Rebase Develop 01
+    // Test rebase Develop 02
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
