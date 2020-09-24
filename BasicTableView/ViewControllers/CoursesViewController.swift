85d647f2f7f05584e8115d63b7726b8371154c85
Cat Binh Le <catbinhle@gmail.com>
2020-09-24 16:38:53 +0700

    Added "Dev4"

--- BasicTableView/ViewControllers/CoursesViewController.swift
+++ BasicTableView/ViewControllers/CoursesViewController.swift
@@ -27,7 +27,8 @@ class CoursesViewController: UIViewController {
             "Tester",
             "QA",
             "PMP",
-            "Dev1"
+            "Dev1",
+            "Dev4"
         ]
     }
 
