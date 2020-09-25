8e0f0c3df28e51dee8b372000cbcf1d2a8cf77e0
Cat Binh Le <catbinhle@gmail.com>
2020-09-25 17:27:03 +0700

    [Dev4] Added AppDelegate

--- BasicTableView/AppDelegate.swift
+++ BasicTableView/AppDelegate.swift
@@ -12,11 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     //Test Rebase Re - Develop 03
+    //Test Re - Develop 4
     //Test Rebase Re - Develop 05
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //Test Rebase Re - Develop 03
+        //Test Re - Develop 4
         //Test Rebase Re - Develop 05
         return true
     }
@@ -27,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         //Test Rebase Re - Develop 03
+        //Test Re - Develop 4
         //Test Rebase Re - Develop 05
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
