d5a954bd4907508a3613bf95ba7e28035c7bad14
Cat Binh Le <catbinhle@gmail.com>
2020-11-08 16:44:06 +0700

    Luong remove comment + added "Luong Demo"

--- BasicTableView/AppDelegate.swift
+++ BasicTableView/AppDelegate.swift
@@ -10,18 +10,8 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    //Test Rebase Re - Develop 02
-    //Test Rebase Re - Develop 03
-    //Test Re - Develop 4
-    //Test Rebase Re - Develop 05
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //Test Rebase Re - Develop 02
-        //Test Rebase Re - Develop 03
-        //Test Re - Develop 4
-        //Test Rebase Re - Develop 05
         return true
     }
 
@@ -30,10 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        //Test Rebase Re - Develop 02
-        //Test Rebase Re - Develop 03
-        //Test Re - Develop 4
-        //Test Rebase Re - Develop 05
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
@@ -45,4 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
+// Luong Demo
 
